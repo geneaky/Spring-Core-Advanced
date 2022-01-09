eb060134bdf52fe1b79599e187d765f84d28f092
geneaky <kaktus418@gmail.com>
2022-01-09 19:36:01 +0900

    templatemethodpattern v2

--- advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -55,4 +55,34 @@ public class TemplateMethodTest {
 
     //then
   }
+
+  @Test
+  public void templateMethodV2() throws Exception{
+    //given
+    AbstractTemplate template1 = new AbstractTemplate() {
+
+      @Override
+      protected void call() {
+        log.info("비즈니스 로직 1 실행");
+      }
+    };
+
+    log.info("클래스 이름1={}", template1.getClass());
+    template1.execute();
+
+    AbstractTemplate template2 = new AbstractTemplate() {
+
+      @Override
+      protected void call() {
+        log.info("비즈니스 로직 2 실행");
+      }
+    };
+
+    log.info("클래스 이름2={}", template2.getClass());
+    template2.execute();
+
+    //when
+
+    //then
+  }
 }
