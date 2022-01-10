e0a08c1a6a4d9fae4fbb6e9b70362e291671590d
geneaky <kaktus418@gmail.com>
2022-01-10 20:23:30 +0900

    decorator pattern 2

--- proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -1,6 +1,7 @@
 package hello.proxy.pureproxy.decorator;
 
 import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import hello.proxy.pureproxy.decorator.code.MessageDecorator;
 import hello.proxy.pureproxy.decorator.code.RealComponent;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,19 @@ public class DecoratorPatternTest {
     //given
     RealComponent realComponent = new RealComponent();
     DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+    client.execute();
+
+    //when
+
+    //then
+  }
+
+  @Test
+  public void decorator1() throws Exception{
+    //given
+    RealComponent realComponent = new RealComponent();
+    MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+    DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
     client.execute();
 
     //when
