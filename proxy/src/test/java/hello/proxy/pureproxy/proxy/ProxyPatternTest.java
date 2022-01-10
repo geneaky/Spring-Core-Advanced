91177904d83d36f1230a22d4b53bb8b55ef233e7
geneaky <kaktus418@gmail.com>
2022-01-10 19:45:08 +0900

    proxy pattern 1

---
+++ proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -0,0 +1,23 @@
+package hello.proxy.pureproxy.proxy;
+
+import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
+import hello.proxy.pureproxy.proxy.code.RealSubject;
+import org.junit.jupiter.api.Test;
+
+public class ProxyPatternTest {
+
+  @Test
+  public void noProxyTest() throws Exception{
+    //given
+    RealSubject realSubject = new RealSubject();
+    ProxyPatternClient client = new ProxyPatternClient(realSubject);
+    client.execute();
+    client.execute();
+    client.execute();
+
+    //when
+
+    //then
+  }
+
+}
